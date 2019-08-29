725892a446a897d4a75ce8311919318b3f8aee09
Vitor Giacomelli <kruxer1985@hotmail.com>
2019-08-29 20:10:18 -0300

    Add migrations

---
+++ TaskSchedulerApi/Migrations/20190829230907_taskmanagerapi.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TaskSchedulerApi.Migrations
+{
+    public partial class taskmanagerapi : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tarefas",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Titulo = table.Column<string>(nullable: true),
+                    Status = table.Column<bool>(nullable: false),
+                    Descricao = table.Column<string>(nullable: true),
+                    DataCriacao = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tarefas", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tarefas");
+        }
+    }
+}
